58fed7814957142248325e28b7721e34b624e1ba
notJAY <notjayanand@gmail.com>
2022-04-14 19:24:45 +0530

    basic

--- pointers.c
+++ pointers.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main()
 {
+    printf("POINTERS\n");
     
+    int a=5;
+    int *ptr= &a;
+    printf("%d\n",a);
+    printf("%x\n",ptr); //%x for hexadecimals %p for pointers
+    printf("%d\n",*ptr);
+    printf("%d\n",&ptr);
+
 }
